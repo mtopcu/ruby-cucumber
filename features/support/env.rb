2f128d161e4f33868e6b3551ebeddfb57e6521b6
Topcuogullari <trtopcmu@TRSEUISTMAC033.local>
2019-10-09 13:05:20 +0300

    selenium grid config

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,11 +3,24 @@ require 'capybara/dsl'
 require 'cucumber'
 require 'selenium-webdriver'
 require 'rspec'
- 
+
+
 Capybara.default_driver = :selenium
 Capybara.app_host = "https://www.sonymobile.com"
 Capybara.register_driver :selenium do |app|
+  
   Capybara::Selenium::Driver.new app, browser: :chrome
 end
-  
+
+=begin // For Selenium Grid
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome
+    Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        url: "http://localhost:4444/wd/hub",
+        desired_capabilities: caps)
+    end
+=end
+
+
 World(Capybara::DSL)
